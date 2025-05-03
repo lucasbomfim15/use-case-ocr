f7ac70706e96f2011c6c7a9f8c45f6bc121ae71e
lucasbomfim15 <bmfmlucas@gmail.com>
2025-05-03 12:55:31 -0300

    refactor(back-end): remove old LLM integration and update documents module

--- backend/src/app/modules/documents/documents.module.ts
+++ backend/src/app/modules/documents/documents.module.ts
@@ -4,11 +4,11 @@ import { DocumentsController } from "./controller/document.controller";
 import { OcrService } from "./services/ocr.service";
 import { PrismaModule } from "src/app/shared/prisma/prisma.module";
 import { AuthModule } from "../auth/auth.module";
-import { LlmModule } from "../llm/llm.module";
+
 import { GroqModule } from "../groq/groq.module";
 
 @Module({
-    imports: [PrismaModule, AuthModule, LlmModule, GroqModule],
+    imports: [PrismaModule, AuthModule, GroqModule],
     controllers: [DocumentsController],
     providers: [DocumentsService, OcrService],
     exports: [],
